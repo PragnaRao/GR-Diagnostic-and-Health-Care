c9a1628345ca39c9de4bed209c85fa4f3055ca1e
PragnaRao <153588427+PragnaRao@users.noreply.github.com>
2023-12-14 17:20:37 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,34 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000);  
+    });
+
+
+    //Navigation Section
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      autoplay:true,
+    })
+
+
+    // PARALLAX EFFECT
+    $.stellar();  
+
+
+
+    // WOW ANIMATION
+    new WOW({ mobile: false }).init();
+
+})(jQuery);
